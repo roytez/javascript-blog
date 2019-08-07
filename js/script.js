8b1224120a71f22cae8334973f291d4aa84b229a
Mateusz Krasinkiewicz <mateuszkrasinkiewicz@gmail.com>
2019-08-07 12:56:22 +0200

    Add console.log

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function titleClickHandler(event){
+  // console.log('Link was clicked!'); (to było wcześniej)
+  console.log('event');  // Cwiczenie pierwsze, nie ma pojęcia czy dobrze zrobione
+
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+  /* add class 'active' to the clicked link */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
