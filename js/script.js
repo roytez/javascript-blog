ad4e828f5268c93cf173c0fdd586a94a44830c9b
Mateusz Krasinkiewicz <mateuszkrasinkiewicz@gmail.com>
2019-08-08 18:32:38 +0200

    Complete function

--- js/script.js
+++ js/script.js
@@ -25,37 +25,23 @@ function titleClickHandler(event){
 }
 
   /* get 'href' attribute from the clicked link */
-  const articleSelector = document.getElementById('.article');
-  articleSelector = articleSelector.getAttribute('href');
+  const articleSelector = clickedElement.getAttribute('href');
   console.log('articleSelector');
-
-
-/*   PRZYKŁAD
- var div1 = document.getElementById("div1");
-  var a = div1.getAttribute("align");
-  alert(a); // pokaże wartość atrybutu "align" dla elementu o id="div1"
-
-            instrukcja
-
-Umiemy już znaleźć kliknięty link – zapisaliśmy go w stałej clickedElement. Znajdziemy teraz jego atrybut href.
-
-Zadeklarujmy nową stałą o nazwie articleSelector. Od razu możemy ustawić jej wartość, czyli wartość atrybutu href pobraną z klikniętego linka.
-
-Spójrz na przykład znajdujący się w dokumentacji funkcji getAttribute – powinien podpowiedzieć Ci, w jaki sposób użyć tej funkcji. Pamiętaj, że musimy ją wykonać na elemencie clickedElement, a w nawiasach podać nazwę atrybutu w cudzysłowach, czyli href.
-
-Użyj console.log dla stałej articleSelector, aby sprawdzić, czy udało się pobrać wartość tego atrybutu. Powinna być inna dla każdego linka, i wyświetlać się w konsoli po kliknięciu w link.
-
-
-  */
-
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector)
+    console.log(targetArticle)
+
+
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+
 }
 
-const links = document.querySelectorAll('.titles a');
+  const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+  for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
